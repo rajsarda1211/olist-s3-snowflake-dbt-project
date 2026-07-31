bbb99c28223d3a7160647db18746d998f7fb6863
Raj Sarda <sardaraj1211@gmail.com>
2026-07-31 11:59:18 +0530

    Revert dim_seller geolocation enrichment - city/state/zip sufficient for business questions, dim_location remains standalone for spatial analysis needs

--- olist_dbt/models/gold/dims/dim_seller.sql
+++ olist_dbt/models/gold/dims/dim_seller.sql
@@ -2,25 +2,19 @@ WITH source AS (
     SELECT * FROM {{ ref('snp_sellers') }}
 ),
 
-location AS (
-    SELECT * FROM {{ ref('stg_geolocation') }}
-),
+
 
 enriched AS (
     SELECT
-        {{ dbt_utils.generate_surrogate_key(['s.seller_id', 's.dbt_valid_from']) }} AS seller_key,
-        s.seller_id,
-        s.seller_city,
-        s.seller_state,
-        s.seller_zip_code_prefix,
-        l.geolocation_lat,
-        l.geolocation_lng,
-        s.dbt_valid_from,
-        s.dbt_valid_to,
-        CASE WHEN s.dbt_valid_to IS NULL THEN TRUE ELSE FALSE END AS is_current
-    FROM source s
-    LEFT JOIN location l
-        ON s.seller_zip_code_prefix = l.geolocation_zip_code_prefix
+        {{ dbt_utils.generate_surrogate_key(['seller_id', 'dbt_valid_from']) }} AS seller_key,
+        seller_id,
+        seller_city,
+        seller_state,
+        seller_zip_code_prefix,
+        dbt_valid_from,
+        dbt_valid_to,
+        CASE WHEN dbt_valid_to IS NULL THEN TRUE ELSE FALSE END AS is_current
+    FROM source
 )
 
 SELECT * FROM enriched
