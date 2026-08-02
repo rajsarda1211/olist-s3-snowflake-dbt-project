02d4e607271580567643ed0a6f4e94a6e2eebe23
Raj Sarda <sardaraj1211@gmail.com>
2026-08-02 11:40:40 +0530

    Fix snp_customers to select zip/city based on most recent order_purchase_timestamp instead of _loaded_at

--- olist_dbt/snapshots/snp_customers.sql
+++ olist_dbt/snapshots/snp_customers.sql
@@ -9,17 +9,30 @@
     )
 }}
 
-WITH deduplicated AS (
+WITH customer_orders AS (
+    SELECT
+        c.customer_unique_id,
+        c.customer_zip_code_prefix,
+        c.customer_city,
+        c.customer_state,
+        c._loaded_at,
+        o.order_purchase_timestamp
+    FROM {{ ref('stg_customers') }} c
+    JOIN {{ ref('stg_orders') }} o
+        ON c.customer_id = o.customer_id
+),
+
+deduplicated AS (
     SELECT
         customer_unique_id,
         customer_zip_code_prefix,
         customer_city,
         customer_state,
         _loaded_at
-    FROM {{ ref('stg_customers') }}
+    FROM customer_orders
     QUALIFY ROW_NUMBER() OVER (
         PARTITION BY customer_unique_id
-        ORDER BY _loaded_at DESC
+        ORDER BY order_purchase_timestamp DESC
     ) = 1
 )
 
